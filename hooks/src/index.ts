7b3c51aabfce562453fa40c80fdfd5e6ef2e382b
imsidkg <siddharth8271skg2@gmail.com>
2024-08-10 00:09:20 +0530

    zapRun and ZapOutboxRun added

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,3 +1,28 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client';
 
-const app = express()
+const client = new PrismaClient();
+const app = express();
+app.use(express.json());
+
+app.post('/hooks/catch/:userId/:zapId' , async(req, res) =>{
+    const body = req.body;
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+
+
+    await client.$transaction(async tx => {
+        const run  =  await tx.zapRun.create({
+            data :{
+                zapId : zapId,
+                metadata : body
+            }
+        })
+
+        await tx.zapRunOutbox.create({
+            data :{
+                zapRunId : zapId
+            }
+        })
+    })     
+})
